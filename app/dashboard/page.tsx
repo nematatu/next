8dbce0174c95104e2493d2abe805cedc1712e66c
nematatu <nematatu1003@gmail.com>
2024-09-26 16:07:33 +0900

    finish

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,30 @@
-export default function Page(){
-    return <p>Dashboard page</p>;
+import RevenueChart from '..//ui/dashboard/revenue-chart';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { CardSkeleton } from '@/app/ui/skeletons';
+import CardWrapper from '../ui/dashboard/cards';
+
+
+export default async function Page() {
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <CardWrapper />
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
+            </div>
+        </main>
+    )
 }
